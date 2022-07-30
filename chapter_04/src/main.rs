a73b2c71c7d38323a74d288dfec03ddb272c6019
Christian Prell <christian.prell@codecamp-n.com>
2022-07-30 09:37:18 +0200

    chap 4.2. done

--- chapter_04/src/main.rs
+++ chapter_04/src/main.rs
@@ -31,4 +31,41 @@ fn main() {
     // this is valid, x is valid since size is known at runtime -> stored on stack
 
     // references and borrowing (chap. 4.2)
+    // when passing a reference there is no ownership -> value it points to will not be dropped
+    // creating a reference is called borrowing
+    // references cannot be changed, unless they are made mutable
+
+    let example_string = String::from("Hello!");
+    // mutable reference would be
+    // let mut example_string = String::from("Hello!");
+
+    let length = calculate_string_length(&example_string);
+    // mutable reference call would be
+    // let length = calculate_string_length(&mut example_string); + calculate_string_length should
+    // accept &mut String as parameter
+
+    println!("String length of string \"{example_string}\" is {length}");
+
+    // this fails
+    // you cannot have 2 mutable references to one value
+    // let mut s = String::from("hello");
+
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+
+    // this prevents data races
+
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+}
+
+fn calculate_string_length(s: &String) -> usize {
+    s.len()
 }
