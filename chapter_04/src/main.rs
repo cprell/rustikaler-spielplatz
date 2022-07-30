d38ab765e4099c03931f23309d16b9daf27f8ede
Christian Prell <christian.prell@codecamp-n.com>
2022-07-30 09:55:03 +0200

    chap 4.3. done

--- chapter_04/src/main.rs
+++ chapter_04/src/main.rs
@@ -64,8 +64,71 @@ fn main() {
 
     let r3 = &mut s; // no problem
     println!("{}", r3);
+
+    // slicing
+
+    let mut s = String::from("Hello world");
+    let word = first_word_old(&s);
+    // s.clear();
+    println!("The first word is: {word}");
+
+    string_slicing_shenanigans();
+
+    // array slicing
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
 }
 
 fn calculate_string_length(s: &String) -> usize {
     s.len()
+}
+
+fn first_word_old(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            // b = byte literal
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn string_slicing_shenanigans() {
+    let my_string = String::from("hello world");
+
+    // `first_word` works on slices of `String`s, whether partial or whole
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // `first_word` also works on references to `String`s, which are equivalent
+    // to whole slices of `String`s
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word` works on slices of string literals, whether partial or whole
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    let word = first_word(my_string_literal);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            // b = byte literal
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
